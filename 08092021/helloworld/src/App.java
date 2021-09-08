f82cb642803307457047ccd27812a91521beb655
qolbi <nzqadii@gmail.com>
2021-09-08 22:38:45 +0700

    08092021.2238

---
+++ 08092021/helloworld/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Saya Belajar Java");
+        int nilai = 2+3;
+        System.out.println("2 + 3 = " + nilai);
+    }
+}
